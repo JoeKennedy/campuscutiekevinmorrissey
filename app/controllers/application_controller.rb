2a3fa9ca961655aae3121da60ba30842a3da950e
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2013-06-03 21:33:24 -0400

    Convert days until Kevin's birthday to Eastern Time Zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,13 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, :alert => exception.message
   end
 
-  helper_method :eastern
+  helper_method :eastern, :eastern_nf
 
   def eastern(dt)
     @dt = dt.in_time_zone("Eastern Time (US & Canada)").strftime('%b %d, %Y %I:%M %p')
+  end
+
+  def eastern_nf(dt)
+    @dt = dt.in_time_zone("Eastern Time (US & Canada)").to_date
   end
 end
